b557763171ffe632fb9d85216dd702e4166a9d62
CHRIS SKYR <christaraskyr@gmail.com>
2022-04-16 23:17:44 +0300

    Fixing Mistakes

--- Assets/Scripts/Items/LightSwitch.cs
+++ Assets/Scripts/Items/LightSwitch.cs
@@ -59,7 +59,7 @@ public class LightSwitch : Interactable
         return "Press [E] to turn <color=green>on</color> the light.";
     }
 
-    public override void Interact()
+    public override void Interaction()
     {
         isOn = !isOn;
         UpdateLight();
